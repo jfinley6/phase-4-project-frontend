0af09e3514ea2b7431550c2838eb1cf2b38771b9
Graham <thegrahamprice@icloud.com>
2022-09-13 10:35:58 -0600

    Pull from main

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,49 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
-function PostCard() {
-  return (
-    <div class="card-group">
-      <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This card has supporting text below as a natural lead-in to
-            additional content.
-          </p>
-          <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" src="..." alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </p>
-          <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </p>
-        </div>
-=======
 function PostCard({ post }) {
   return (
     <div className="col-sm-3 d-flex justify-content-center align-items-center flex-column border mb-3">
@@ -65,7 +21,6 @@ function PostCard({ post }) {
           </small>
         </p> */}
         <button className="btn btn-primary">Read</button>
->>>>>>> main
       </div>
     </div>
   );
