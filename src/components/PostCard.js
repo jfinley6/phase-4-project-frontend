fb759ee436df957b774a0f254ae8159531409969
johnfinley <johnfinleybremerton@gmail.com>
2022-09-13 09:53:38 -0600

    card css

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,21 +2,10 @@ import React from "react";
 
 function PostCard({ post }) {
   return (
-    // <div className="card d-flex justify-content-center align-items-center flex-column">
-    //   <img className="h-50 w-50" src={post.image_url} alt="Card image cap"/>
-    //   <div className="card-body d-flex flex-column align-items-center">
-    //     <h5 className="card-title">{post.subject}</h5>
-    //     <p className="card-text"><small className="text-muted">Created At: {post.created_at.slice(0, -14)}</small></p>
-    //     <button onClick={() => {
-    //       console.log(post.id)
-    //     }} className='btn btn-primary'>Read</button>
-    //   </div>
-    // </div>
-
-    <div className="col-sm-3 d-flex flex-column border mb-3">
+    <div className="col-sm-3 d-flex justify-content-center align-items-center flex-column border mb-3">
       <img
         src={post.image_url}
-        className="img-responsive card-img-top mt-3 h-50"
+        className="card-img-top mt-3 w-50 h-50"
         alt="..."
       />
       <div className="card-body d-flex flex-column align-items-center">
@@ -26,6 +15,11 @@ function PostCard({ post }) {
             Created at: {post.created_at.slice(0, -14)}
           </small>
         </p>
+        {/* <p className="card-text">
+          <small className="text-muted">
+            0 likes | 0 comments
+          </small>
+        </p> */}
         <button className="btn btn-primary">Read</button>
       </div>
     </div>
