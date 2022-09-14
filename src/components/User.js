2ccfd6ba0096d656da7b8ed83757313c420dbcdc
Graham <thegrahamprice@icloud.com>
2022-09-14 13:42:11 -0600

    the changes that didn't work just yet...

--- src/components/User.js
+++ src/components/User.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 // import {} from "react-icons";
 import axios from "axios";
+import "../style/User.css";
 
 function User({ user }) {
   const history = useHistory();
+
   const [userName, setUserName] = useState({
     username: "",
   });
@@ -15,30 +17,28 @@ function User({ user }) {
       [event.target.username]: event.target.value,
     });
   }
-  // // function handleSubmit(event) {
-  // //   axios
-  // //     .post(
-  // //       "http://localhost:3001/users",
-  // //       {
-  // //         user: {
-  // //           username: username,
-  // //           // email: email,
-  // //           // password: password,
-  // //           // password_confirmation: password_confirmation,
-  // //         },
-  // //       },
-  // //       { withCredentials: true }
-  //     )
-  //     .then((response) => {
-  //       if (response.data.status === "created") {
-  //         handleSuccessfulAuth(response.data.user);
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log("registration error", error);
-  //     });
-  //   event.preventDefault();
-  // const [email, password_digest] = user;
+  function handleSubmit(event) {
+    axios
+      .patch(
+        "http://localhost:3001/registrations",
+        {
+          user: {
+            username: user.username,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        if (response.data.status === "created") {
+          handleSuccessfulAuth(response.data.user);
+        }
+      })
+      .catch((error) => {
+        console.log("YOU the best", error);
+      });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <link
@@ -51,9 +51,10 @@ function User({ user }) {
             <div className="panel panel-default">
               <div className="panel-body text-center">
                 <img
-                  src="https://media.istockphoto.com/vectors/default-avatar-profile-icon-vector-vector-id1337144146?b=1&k=20&m=1337144146&s=170667a&w=0&h=ys-RUZbXzQ-FQdLstHeWshI4ViJuEhyEa4AzQNQ0rFI="
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
                   className="img-circle profile-avatar"
                   alt="User avatar"
+                  id="profile-pic"
                 />
               </div>
             </div>
@@ -63,51 +64,25 @@ function User({ user }) {
               </div>
             </div>
             <div className="panel-body">
-              <form className="form-group-1">
+              <span className="form-group-1">
                 <label className="col-sm-2 control-label">Username</label>
                 <h4 className="col-md-10 control-label">{user.username}</h4>
-              <div className="form-group">
-                <label className="col-sm-2 control-label">E-mail</label>
-                <div className="col-sm-10">
-                  <input type="email" className="form-control" value={user.email} />
-                </div>
-              </div>
-              <div className="form-group"></div>
-            </div>
-          </div>
-
-          <div className="panel panel-default">
-            <div className="panel-heading">
-              <h4 className="panel-title">Security</h4>
-            </div>
-            <div className="panel-body">
-              <div className="form-group">
-                <label className="col-sm-2 control-label">
-                  Current password
-                </label>
-                <div className="col-sm-10">
-                  {/* <label className="col-sm-2 control-label">
-                    {user.username}
-                  </label> */}
+                <div className="form-group">
                   <input
                     type="username"
                     className="form-control"
                     onChange={handleChange}
-                    //
                     placeholder="New Username"
+                    defaultValue="New Username"
+                    // value={user.username}
                   />
                 </div>
-              </form>
+              </span>
               <div className="panel-body">
                 <div className="form-group-2">
                   <label className="col-sm-2 control-label">E-mail</label>
                   <div className="col-sm-10">
                     <h4>{user.email}</h4>
-                    {/* <input
-                    type="email"
-                    className="form-control"
-                    onChange={handleChange}
-                  /> */}
                   </div>
                 </div>
                 <div className="form-group"></div>
@@ -125,10 +100,9 @@ function User({ user }) {
                   </label>
                   <div className="col-sm-10">
                     <input
-                      // onChange={console.log("change")}
                       type="password"
                       className="form-control"
-                      value=""
+                      defaultValue="Password"
                     />
                   </div>
                 </div>
@@ -138,8 +112,7 @@ function User({ user }) {
                     <input
                       type="password"
                       className="form-control"
-                      // onChange={console.log("change")}
-                      value="new-password"
+                      defaultValue="new-password"
                     />
                   </div>
                 </div>
@@ -151,8 +124,7 @@ function User({ user }) {
                     <input
                       type="password"
                       className="form-control"
-                      // onChange={console.log("change")}
-                      value="confirm-password"
+                      defaultValue="confirm-password"
                     />
                   </div>
                 </div>
@@ -161,7 +133,11 @@ function User({ user }) {
                 </div>
                 <div className="form-group">
                   <div className="col-sm-10 col-sm-offset-2">
-                    <button type="submit" className="btn btn-primary">
+                    <button
+                      type="submit"
+                      className="btn btn-primary"
+                      onClick={handleSubmit}
+                    >
                       Submit
                     </button>
                     <button type="reset" className="btn btn-default">
