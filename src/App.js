e571dd8c693fa132d3038f923c94214485ab9169
johnfinley <johnfinleybremerton@gmail.com>
2022-09-16 19:50:29 -0600

    app page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import axios from "axios";
+
+//Components
+import Home from "./components/Home"
+import Content from "./components/Content";
+import NavBar from "./components/NavBar";
+import NewPost from "./components/NewPost";
+import User from "./components/User";
+import PostDetail from "./components/PostDetail";
 
 function App() {
+   const [loggedInStatus, setLoggedInStatus] = useState("NOT_LOGGED_IN");
+   const [user, setUser] = useState({});
+   const [screen, setScreen] = useState(false);
+   const [storedPost, setStoredPost] = useState("");
+   const [storedSubject, setStoredSubject] = useState("");
+
+   const history = useHistory();
+
+   useEffect(() => {
+     checkLoginStatus();
+   }, []);
+
+   function checkLoginStatus() {
+     axios
+       .get("https://radiant-atoll-92288.herokuapp.com/logged_in", {
+         withCredentials: true,
+       })
+       .then((response) => {
+         console.log(response);
+         if (response.data.logged_in && loggedInStatus === "NOT_LOGGED_IN") {
+           setLoggedInStatus("LOGGED_IN");
+           setUser(response.data.user);
+           history.push("/");
+         } else if (
+           !response.data.logged_in &&
+           loggedInStatus === "LOGGED_IN"
+         ) {
+           setLoggedInStatus("NOT_LOGGED_IN");
+           setUser({});
+         } else if (!response.data.logged_in) {
+           setUser({});
+           history.push("/");
+         }
+       })
+       .catch((error) => console.log(error));
+   }
+
+   function handleLogin(data) {
+     setLoggedInStatus("LOGGED_IN");
+     setUser(data);
+   }
+
+   function handleLogout() {
+     axios
+       .delete("https://radiant-atoll-92288.herokuapp.com/logout", {
+         withCredentials: true,
+       })
+       .then(() => {
+         setLoggedInStatus("NOT_LOGGED_IN");
+         setUser({});
+         history.push("/");
+       })
+       .catch((error) => console.log(error));
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app d-flex flex-column justify-content-center">
+      <NavBar
+        loggedInStatus={loggedInStatus}
+        setScreen={setScreen}
+        handleLogout={handleLogout}
+        user={user}
+      />
+      <Switch>
+        <Route exact path={"/"}>
+          <Content loggedInStatus={loggedInStatus} user={user} />
+        </Route>
+        <Route exact path={"/home"}>
+          <Home
+            screen={screen}
+            setScreen={setScreen}
+            loggedInStatus={loggedInStatus}
+            setLoggedInStatus={setLoggedInStatus}
+            handleLogin={handleLogin}
+          />
+        </Route>
+
+        <Route exact path={"/new"}>
+          <NewPost
+            user={user}
+            storedPost={storedPost}
+            setStoredPost={setStoredPost}
+            setStoredSubject={setStoredSubject}
+            storedSubject={storedSubject}
+          />
+        </Route>
+
+        <Route exact path={"/user"}>
+          <User user={user} setUser={setUser} />
+        </Route>
+        <Route exact path="/posts/:id">
+          <PostDetail user={user} loggedInStatus={loggedInStatus} />
+        </Route>
+      </Switch>
     </div>
   );
 }
